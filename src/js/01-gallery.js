e51c311e56c2b6c98f45e87a3c6f85bbeedbba97
AlexandrOAV <a0617011995@gmail.com>
2023-05-26 15:53:10 +0300

    uodate 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,8 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
-// Описаний в документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 const listEl = document.querySelector ('.gallery');
 const addMarkup = creatingListPictures(galleryItems);
 
