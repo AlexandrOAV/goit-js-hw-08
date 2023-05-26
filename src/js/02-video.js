83e6c2d0132c86d896ae6bcb6bacf718db29c4e8
AlexandrOAV <a0617011995@gmail.com>
2023-05-26 15:47:59 +0300

    tasc-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,16 +4,24 @@ import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
 const savedTime = localStorage.getItem('videoplayer-current-time')
-
+reloadPage(savedTime);
 player.on('timeupdate', throttle(function (timeupdate) {
-    console.log(timeupdate.seconds);
     let currentTime = timeupdate.seconds;
     localStorage.setItem('videoplayer-current-time', currentTime);
 }, 1000));
-reloadPage(savedTime);
-function reloadPage(time){
-if (time) {
-    player.setCurrentTime(time);
+
+function reloadPage(time) {
+    
+    if (time) {
+        player.setCurrentTime(time).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+        });
+        
     }
 };
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
+
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle((inputOfForm), 500));   
+formEl.addEventListener('submit', submitForm);
+reloadPageForm();
+function inputOfForm() {
+let feedbackUser = {
+    email: inputEl.value,
+    massage: textareaEl.value,
+  }
+    saveStorage(feedbackUser);   
+    return feedbackUser;
+} 
+function saveStorage(object) {
+    let addElementStorage = JSON.stringify(object);
+    localStorage.setItem('feedback-form-state', addElementStorage);
+    let objectLocalStorage = localStorage.getItem('feedback-form-state');
+}
+
+function reloadPageForm() {
+    let objectJson = localStorage.getItem('feedback-form-state')
+    try {
+    let loadObjectStorage = JSON.parse(objectJson);
+    if (!loadObjectStorage) {
+        return
+    } 
+        inputEl.value = loadObjectStorage.email;
+        textareaEl.value = loadObjectStorage.massage;
+        } catch (error){
+        console.error('Initial data error ');
+        localStorage.clear('feedback-form-state')
+    }
+   
+}
+function submitForm(event) {
+    event.preventDefault();
+    const submitUser = {
+        email: inputEl.value,
+        massage: textareaEl.value,
+    }
+    console.log('User:', submitUser);
+
+    inputEl.value = '';
+    textareaEl.value = '';
+    localStorage.removeItem('feedback-form-state');
+    // ще як варіант можна повністю очистити сховище через  clear()
+}
+
 
